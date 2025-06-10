ca6e137afd522c8473decd2b150c54ff620e5197
Kacper <s198186@student.pg.edu.pl>
2025-06-10 19:49:05 +0200

    backend poprawki

--- Encryption.py
+++ Encryption.py
@@ -1,6 +1,7 @@
 from Crypto.Cipher import AES
 from Crypto.Random import get_random_bytes
 from Crypto.Util.Padding import pad, unpad
+from hashlib import sha256
 import os
 import secrets
 #os.chdir(os.path.dirname(os.path.abspath(__file__)))
@@ -28,22 +29,22 @@ def encryption(filepath, key, mode):
     except Exception as e:
         raise ValueError(f"BLAD: {e}")
 
-    key = key.ljust(32, b'\0')[:32]
+    key = sha256(key).digest() #key = sha256(key.encode()).digest() dla stringa
     iv = get_random_bytes(16)
 
     if mode == 'ECB':
         cipher = AES.new(key, AES.MODE_ECB)
         ciphertext = cipher.encrypt(padding(input))
-        output = ciphertext
+        output = b'B' + ciphertext
     elif mode == 'CBC':
         cipher = AES.new(key, AES.MODE_CBC, iv)
         ciphertext = cipher.encrypt(padding(input))
-        output = iv + ciphertext
+        output = b'C' + iv + ciphertext
     elif mode == 'CTR':
         nonce = iv[:8]
         cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
         ciphertext = cipher.encrypt(input)
-        output = nonce + ciphertext
+        output = b'R' + nonce + ciphertext
     else:
         raise ValueError("NIEZNANY TRYB")
 
@@ -61,17 +62,20 @@ def decryption(filepath, key, mode):
     except Exception as e:
         raise ValueError(f"BLAD: {e}")
 
-    key = key.ljust(32, b'\0')[:32]
+    key = sha256(key).digest()     #key = sha256(key.encode()).digest() dla stringa
 
-    if mode == 'ECB':
+    mode = chr(input[0])
+    input = input[1:]
+
+    if mode == 'B':
         cipher = AES.new(key, AES.MODE_ECB)
         output = unpadding(cipher.decrypt(input))
-    elif mode == 'CBC':
+    elif mode == 'C':
         iv = input[:16]
         ciphertext = input[16:]
         cipher = AES.new(key, AES.MODE_CBC, iv)
         output = unpadding(cipher.decrypt(ciphertext))
-    elif mode == 'CTR':
+    elif mode == 'R':
         nonce = input[:8]
         ciphertext = input[8:]
         cipher = AES.new(key, AES.MODE_CTR, nonce=nonce)
